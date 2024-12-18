4efd3bfd0e8793b940e5797f7f5c1b03f1fc2933
SidneyPlt <speault@gaming.tech>
2024-12-18 17:47:07 +0100

    jsp

--- src/ShootEmUp/PauseMenu.cpp
+++ src/ShootEmUp/PauseMenu.cpp
@@ -1,5 +1,6 @@
 #include "PauseMenu.h"
 #include "Debug.h"
+#include <SFML/Graphics.hpp>
 
 PauseMenu::PauseMenu(sf::RenderWindow* window)
 {
@@ -15,6 +16,27 @@ PauseMenu::PauseMenu(sf::RenderWindow* window)
 
     // Dessiner le conteneur
     Debug::DrawFilledRectangle(containerX, containerY,containerWidth, containerHeight,sf::Color(255, 255, 255, 255)); 
+
+    // Créer un bouton (rectangle)
+    sf::RectangleShape button(sf::Vector2f(100, 50)); // Taille du bouton
+    button.setPosition(350, 275);                    // Position dans la fenêtre
+    button.setFillColor(sf::Color::Blue);            // Couleur du bouton
+
+    // Détecter un clic sur le bouton
+    /*if (event.type == sf::Event::MouseButtonPressed) {
+        if (event.mouseButton.button == sf::Mouse::Left) { // Clic gauche
+            sf::Vector2i mousePos = sf::Mouse::getPosition(window); // Position de la souris
+            if (button.getGlobalBounds().contains(mousePos.x, mousePos.y)) {
+                // Basculer le son
+                if (sound.getStatus() == sf::Sound::Playing) {
+                    sound.pause(); // Arrête le son
+                }
+                else {
+                    sound.play(); // Relance le son
+                }
+            }
+        }
+    }*/
 }
 
 
@@ -27,7 +49,7 @@ PauseMenu::~PauseMenu()
 
 void PauseMenu::update()
 {
-
+   
 
 }
 
--- src/ShootEmUp/pch.cpp
+++ src/ShootEmUp/pch.cpp
@@ -1,2 +1,40 @@
 #include "pch.h"
+#include "Debug.h"
+#include "BTP.h"
+#include "Camion.h"
+#include "Collider.h"
+#include "ColliderManager.h"
+#include "DummyEntity.h"
+#include "Entity.h"
+#include "EntityDesign.h"
+#include "GameManager.h"
+#include "GameScene.h"
+#include "GoFast.h"
+#include "main.h"
+#include "Menu.h"
+#include "PauseMenu.h"
+#include "Player.h"
+#include "Pompier.h"
+#include "Projectile.h"
+#include "resource.h"
+#include "ResourceManager.h"
+#include "SampleScene.h"
+#include "Scene.h"
+#include "Utils.h"
+
+#include<iostream>
+#include<ctime>
+#include<cstdlib>
+#include<fstream>
+#include<sstream>
+#include<vector>
+#include<stack>
+#include<map>
+
+
+#include "SFML\System.hpp"
+#include "SFML\window.hpp"
+#include "SFML\Graphics.hpp"
+#include "SFML\Audio.hpp"
+#include "SFML\Network.hpp"
 
